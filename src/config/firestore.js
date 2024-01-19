ed938e791081bdc2a7b7c9159c0ebd23c3093f2c
samy <samyessam2000@gami.com>
2024-01-19 18:31:03 +0200

    update

---
+++ src/config/firestore.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzXPAT5TmraSO4llqiCfjXHuZCXW5MqQk",
+  authDomain: "shop-dashbourd.firebaseapp.com",
+  projectId: "shop-dashbourd",
+  storageBucket: "shop-dashbourd.appspot.com",
+  messagingSenderId: "297520425761",
+  appId: "1:297520425761:web:19dac0d4f37422da102c60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
